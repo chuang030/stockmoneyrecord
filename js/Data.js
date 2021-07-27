fd90bdf9c8bbd47950f61a49f37229918aa2a73b
chuang030 <60647082+chuang030@users.noreply.github.com>
2021-07-27 20:28:44 +0800

    initial commit

---
+++ js/Data.js
@@ -0,0 +1,347 @@
+/**
+ * 設置查詢連結
+ * @param {Number|String} date 輸入日期，EX:2021年1月1日則輸入20210101
+ * @param {Number|String} stockNo 輸入股票編號
+ * @returns 查詢連結
+ */
+function setUrl(date, stockNo) {
+    return `https://cors-anywhere.herokuapp.com/https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${date}&stockNo=${stockNo}&_=1498059436321`;
+}
+
+
+Vue.createApp({
+    data: function () {
+        return {
+            hintState: "",
+            hintMessage: "",
+            selectToday: this.getToday(),
+            fullDateTW: "",
+            item: [
+                {
+                    no: 0,
+                    sName: "中鋼",
+                    date: this.getFullToday(),
+                    stockNo: 2002
+                },
+                {
+                    no: 1,
+                    sName: "長榮",
+                    date: this.getFullToday(),
+                    stockNo: 2603
+                },
+                {
+                    no: 2,
+                    sName: "萬海",
+                    date: this.getFullToday(),
+                    stockNo: 2615
+                },
+                {
+                    no: 3,
+                    sName: "聯詠",
+                    date: this.getFullToday(),
+                    stockNo: 3034
+                }
+            ],
+            account: [
+                {
+                    averagePrice: 39.85,
+                    numberOfPiles: 400
+                },
+                {
+                    averagePrice: 177.84,
+                    numberOfPiles: 260
+                },
+                {
+                    averagePrice: 281.54,
+                    numberOfPiles: 90
+                },
+                {
+                    averagePrice: 503.00,
+                    numberOfPiles: 30
+                },
+            ],
+            dataObjClass: [
+                {
+                    date: [],
+                    marketPrice: [],
+                    averagePrice: [],
+                    numberOfPiles: [],
+                    marketValue: [],
+                    carryingCosts: [],
+                    anticipatedRevenue: [],
+                    profitAndLoss: [],
+                    profitAndLossPercentage: [],
+                },
+                {
+                    date: [],
+                    marketPrice: [],
+                    averagePrice: [],
+                    numberOfPiles: [],
+                    marketValue: [],
+                    carryingCosts: [],
+                    anticipatedRevenue: [],
+                    profitAndLoss: [],
+                    profitAndLossPercentage: [],
+                },
+                {
+                    date: [],
+                    marketPrice: [],
+                    averagePrice: [],
+                    numberOfPiles: [],
+                    marketValue: [],
+                    carryingCosts: [],
+                    anticipatedRevenue: [],
+                    profitAndLoss: [],
+                    profitAndLossPercentage: [],
+                },
+                {
+                    date: [],
+                    marketPrice: [],
+                    averagePrice: [],
+                    numberOfPiles: [],
+                    marketValue: [],
+                    carryingCosts: [],
+                    anticipatedRevenue: [],
+                    profitAndLoss: [],
+                    profitAndLossPercentage: [],
+                },
+            ]
+
+
+        }
+    },
+    methods: {
+        /**
+         * 資料撈取方法
+         * @param {Array} arrar 
+         * @param {Number} index 
+         * @returns 資料陣列
+         */
+        itemDataFun(arrar = [], index) {
+            let arr = [];
+            for (i = 0; i < arrar.length; i++) {
+                arr.push(arrar[i][index]);
+            }
+            return arr;
+        },
+        /**
+         * 傳入日期(yyyymmdd)取得對應星期
+         * @param {String} date 
+         * @returns 星期
+         */
+        judgeDay(date) {
+            return new Date(date).getDay();
+        },
+        /**
+         * 日期塞選方法(跳六、日)
+         * @param {Number|String} date 
+         * @param {String} fullDate 
+         * @returns 跳躍後日期(dd)
+         */
+        jumpDay(date, fullDate) {
+            let today = new Date();
+            //預留星期變數
+            let todayDay = 0;
+            //預留日變數
+            let day = 0;
+            if (typeof date !== "undefined") {
+                //指定為傳入的日
+                day = Number(date);
+                //取得傳入日期為星期幾
+                todayDay = this.judgeDay(fullDate)
+            } else {
+                //指定為今天的日
+                day = today.getDate();
+                //取得今天為星期幾
+                todayDay = today.getDay();
+            }
+            //判斷是否為星期六、日，是的話推算回星期五的日期(dd)
+            if (todayDay == 6) {
+                if (day - 1 < 10) {
+                    return `0${day - 1}`;
+                } else {
+                    return `${day - 1}`;
+                }
+            } else if (todayDay == 0) {
+                if (day - 2 < 10) {
+                    return `0${day - 2}`;
+                } else {
+                    return `${day - 2}`;
+                }
+            } else {
+                return `${day}`;
+            }
+        },
+        /**
+         * 取得當前民國完整日期(yyy/mm/dd)，並設定this.selectToday值
+         * @returns 民國完整日期(yyy/mm/dd)
+         */
+        getToday() {
+            let today = new Date();
+            let month = "";
+            if (today.getMonth() + 1 < 10) {
+                month = `0${today.getMonth() + 1}`
+            } else {
+                month = today.getMonth() + 1;
+            }
+            this.selectToday = `${today.getFullYear() - 1911}/${month}/${this.jumpDay()}`;
+            return `${today.getFullYear() - 1911}/${month}/${this.jumpDay()}`;
+        },
+        /**
+         * 取得當前西元完整日期(yyyy/mm/dd)，並設定this.selectFullToday值
+         */
+        getFullToday() {
+            let today = new Date();
+            let month = "";
+            if (today.getMonth() + 1 < 10) {
+                month = `0${today.getMonth() + 1}`
+            } else {
+                month = today.getMonth() + 1;
+            }
+            this.selectFullToday = `${today.getFullYear()}${month}${this.jumpDay()}`;
+        },
+        /**
+         * 股票計算方法
+         * @param {Array} marketPrice 輸入撈出的市價陣列
+         * @param {Number} sharePrice 輸入成交價
+         * @param {Number} numberOfPiles 輸入股數
+         * @param {Number} chargeDiscount 輸入手續費折扣
+         * @param {Number|String} type 輸入股票類別
+         * @param {Number} index 輸入欲推送資料之位置
+         */
+        stockCalculation(marketPrice = [], sharePrice, numberOfPiles, chargeDiscount, type, index) {
+            let marketPricePush = [];
+            let averagePricePush = [];
+            let numberOfPilesPush = [];
+            let marketValuePush = [];
+            let carryingCostsPush = [];
+            let anticipatedRevenuePush = [];
+            let profitAndLossPush = [];
+            let profitAndLossPercentagePush = [];
+            let marketPriceArr = marketPrice[this.todayIndex];
+            let sc = new StockCalculation(marketPriceArr, sharePrice, numberOfPiles, chargeDiscount, type);
+            marketPricePush.push(sc.getMarketPrice());
+            averagePricePush.push(sc.getAveragePrice());
+            numberOfPilesPush.push(sc.getNumberOfPiles());
+            marketValuePush.push(sc.getMarketValue());
+            carryingCostsPush.push(sc.getCarryingCosts());
+            anticipatedRevenuePush.push(sc.getAnticipatedRevenue());
+            profitAndLossPush.push(sc.getProfitAndLoss());
+            profitAndLossPercentagePush.push(sc.getProfitAndLossPercentage());
+            this.dataObjClass[index].marketPrice = marketPricePush;
+            this.dataObjClass[index].averagePrice = averagePricePush;
+            this.dataObjClass[index].numberOfPiles = numberOfPilesPush;
+            this.dataObjClass[index].marketValue = marketValuePush;
+            this.dataObjClass[index].carryingCosts = carryingCostsPush;
+            this.dataObjClass[index].anticipatedRevenue = anticipatedRevenuePush;
+            this.dataObjClass[index].profitAndLoss = profitAndLossPush;
+            this.dataObjClass[index].profitAndLossPercentage = profitAndLossPercentagePush;
+        },
+        /**
+         * 各類陣列處理方法
+         * @param {Number} index 
+         * @param {String} todayStr 
+         */
+        arrFun(index = 0, todayStr) {
+            let datePush = [];
+            //todayStr有傳入時取得傳入日期在陣列(撈出資料的陣列)中的位置，反之以今日日期在陣列中的位置
+            todayStr = (typeof todayStr !== "undefined") ? this.todayIndex = this.itemDataFun(this.dataOriginalObject, 0).indexOf(todayStr) : this.todayIndex = this.itemDataFun(this.dataOriginalObject, 0).indexOf(this.selectToday);
+            //將指定位置日期撈出
+            datePush.push(this.itemDataFun(this.dataOriginalObject, 0)[this.todayIndex]);
+            //在指定位置推送撈出之日期
+            this.dataObjClass[index].date = datePush;
+            //取得資料傳入股票計算方法
+            this.stockCalculation(this.itemDataFun(this.dataOriginalObject, 6), this.account[index].averagePrice, this.account[index].numberOfPiles, 6, 0, index);
+        },
+        /**
+         * 民國日期轉換西元日期(yyy/mm/dd) => (yyyymmdd)
+         * @param {String} toDate 傳入民國日期(yyy/mm/dd)
+         */
+        toDate(toDate) {
+            let yyyy = Number(toDate.substring(0, 3)) + 1911;
+            let mm = Number(toDate.substring(4, 6));
+            let dd = Number(toDate.substring(7));
+            if (mm < 10) {
+                mm = `0${mm}`;
+            }
+            if (dd < 10) {
+                dd = `0${dd}`;
+            }
+            let fullDate = `${yyyy}/${mm}/${dd}`;
+            //判斷取得日期為星期幾，並判斷是否跳過，回傳判斷後的日期(日)
+            dd = this.jumpDay(dd, fullDate);
+            if (Number(dd) < 1) {
+                let newDate = new Date(yyyy, Number(mm) - 1, dd);
+                mm = newDate.getMonth() + 1;
+                dd = newDate.getDate();
+                if (mm < 10) {
+                    mm = `0${mm}`;
+                }
+                if (dd < 10) {
+                    dd = `0${dd}`;
+                }
+            }
+            //保留原本日期格式(查詢陣列中的index)
+            let fullDateTW = `${yyyy - 1911}/${mm}/${dd}`;
+            this.fullDateTW = fullDateTW;
+            this.changeDay(`${yyyy}${mm}${dd}`, fullDateTW);
+        },
+        /**
+         * 更換查詢日期，並送出請求後執行後續資料處理
+         * @param {String} date 輸入日期(yyyymmdd)，可不傳入，預設為今日西元日期(yyyymmdd)
+         * @param {String} taiwanYear 輸入日期(yyy/mm/dd)，可不傳入，預設為今日民國日期(yyy/mm/dd)
+         */
+        changeDay(date, taiwanYear) {
+
+            this.item.forEach(element => {
+                setTimeout( () => {
+                    element.date = date;
+                    let requestURL = setUrl(element.date, element.stockNo);
+                    let request = new XMLHttpRequest();
+                    request.open("GET", requestURL, true);
+                    request.responseType = "json";
+                    request.send();
+                    request.onreadystatechange = () => {
+                        if (request.readyState === 4 && request.status === 200) {
+                            this.dataOriginalObject = request.response.data;
+                            iframe.style.display = "none";
+                            button.style.display = "none";
+                            this.arrFun(element.no, taiwanYear);
+                            this.hintState = "成功";
+                        }
+                        if (request.readyState === 4 && request.status === 429) {
+                            iframe.style.display = "none";
+                            button.style.display = "none";
+                            this.hintState = "失敗";
+                            this.hintMessage = "過多請求";
+                        }
+                        if (request.readyState === 4 && request.status === 403) {
+                            this.hintState = "失敗";
+                            this.hintMessage = "請確認是否有點擊啟動按鈕，若有請回報錯誤~~";
+
+                        }
+                        if (request.readyState === 4 && request.status === 0) {
+                            this.hintState = "失敗";
+                            this.hintMessage = "請確認是否有點擊啟動按鈕，若有請回報錯誤~~";
+
+                        }
+                    }
+                }, 500 * element.no);
+
+            });
+        }
+    },
+    mounted() {
+        this.changeDay();
+    }
+}).mount('#app')
+// console.log(request.status);
+const app = document.getElementById("app");
+const iframe = document.getElementsByTagName("iframe")[0];
+const button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", function () {
+    history.go(0);
+})
+
+
+
---
+++ js/StockCalculation.js
@@ -0,0 +1,267 @@
+/**
+ * 股價計算類別，計算公式為元大證卷計算公式
+ * @link https://www.yuanta.com.tw/eYuanta/securities/aporder/Instructions/836878aa-5e5f-4dc8-9d18-984e9bf5c1cd?TargetId=8bc55154-c7ef-4f09-9e6d-e65a8ceb0f39&TargetMode=1
+ */
+class StockCalculation {
+
+    /**
+     * 股價計算類別建構子
+     * @param {Number} marketPrice 輸入市價，預設為1元
+     * @param {Number} sharePrice 輸入成交價，預設為1元
+     * @param {Number} numberOfPiles 輸入購買股數，預設為1股
+     * @param {Number} chargeDiscount 輸入手續費折扣，ex:手續費6折，輸入6，預設值為10(無折扣)
+     * @param {Number|String} type 輸入股票類別，預設值為編號0：普通股票Common Stocks(縮寫CS)，輸入中文、英文全名、英文縮寫、編號皆可);，編號0：普通股票Common Stocks(縮寫CS)、編號1：當沖Day-Trading(縮寫DT)、編號2：指數股票型基金Exchange Traded Funds(縮寫ETF)
+     */
+     constructor(marketPrice = 1, sharePrice = 1, numberOfPiles = 1, chargeDiscount = 10, type = 0) {
+        this.marketPrice = marketPrice;
+        this.sharePrice = sharePrice;
+        this.numberOfPiles = numberOfPiles;
+        this.chargeDiscount = chargeDiscount;
+        this.type = type;
+    }
+
+    /**
+     * 設定成交價
+     * @param {Number} sharePrice 輸入成交價
+     */
+    setCost(sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+    /**
+     * 取得成交價
+     * @returns {Number} 成交價
+     */
+    getCost() {
+        return this.sharePrice;
+    }
+
+    /**
+     * 設定股數
+     * @param {Number} numberOfPiles 輸入購買股數
+     */
+    setNumberOfPiles(numberOfPiles) {
+        this.numberOfPiles = numberOfPiles;
+    }
+
+    /**
+     * 取得股數
+     * @returns 股數
+     */
+    getNumberOfPiles() {
+        return this.numberOfPiles;
+    }
+
+    /**
+     * 設定購買價金(市價、股數)
+     * @param {Number} sharePrice 輸入成交價
+     * @param {Number} numberOfPiles 輸入購買股數
+     */
+    setCostPrice(sharePrice, numberOfPiles) {
+        this.sharePrice = sharePrice;
+        this.numberOfPiles = numberOfPiles;
+    }
+
+    /**
+     * 取得購買價金(市價*股數)
+     * @returns 購買價金
+     */
+    getCostPrice() {
+        return this.sharePrice * this.numberOfPiles;
+    }
+
+    /**
+     * 設定市價
+     * @param {Number} marketPrice 輸入市值
+     */
+    setMarketPrice(marketPrice) {
+        this.marketPrice = marketPrice;
+    }
+
+    /**
+     * 取得市價
+     * @returns 市價
+     */
+     getMarketPrice() {
+        return this.marketPrice;
+    }
+
+    /**
+     * 設定市值
+     * @param {Number} marketPrice 輸入市值
+     * @param {Number} marketPrice 輸入市值
+     */
+    setMarketValue(marketPrice, numberOfPiles) {
+        this.marketPrice = marketPrice;
+        this.numberOfPiles = numberOfPiles;
+    }
+
+    /**
+     * 取得市值(市價*股數)
+     * @returns 市值
+     */
+    getMarketValue() {
+        return Math.round(this.marketPrice * this.numberOfPiles);
+    }
+
+    /**
+     * 設定手續費折扣
+     * @param {Number} chargeDiscount 輸入手續費折扣，ex:手續費6折，輸入6，預設值為10(無折扣)
+     */
+    setChargeDiscount(chargeDiscount) {
+        chargeDiscount = (typeof chargeDiscount !== "undefined") ? chargeDiscount : 10;
+        this.chargeDiscount = chargeDiscount;
+    }
+
+    /**
+     * 取得手續費，取個位數無條件捨去(買進、賣出成交價金x1.425x手續費折扣/1000)
+     * 如果手續費金額小於1(即為0)則以最低手續費計算，最低手續費為1元新台幣
+     * @returns 手續費
+     */
+    getCharge() {
+        let charge = Math.floor((this.getCostPrice() * (1.425 * this.chargeDiscount / 10 / 1000) * 100000) / 100000);
+        return charge == 0 ? 1 : charge;
+    }
+
+    /**
+     * 設定股票類別
+     * @param {Number|String} type 輸入股票類別，預設值為編號0：普通股票Common Stocks(縮寫CS)，輸入中文、英文全名、英文縮寫、編號皆可);，編號0：普通股票Common Stocks(縮寫CS)、編號1：當沖Day-Trading(縮寫DT)、編號2：指數股票型基金Exchange Traded Funds(縮寫ETF)
+     */
+    setType(type) {
+        type = (typeof type !== "undefined") ? type : 0;
+        this.type = type;
+    }
+
+    /**
+     * 取得股票類別
+     * @returns 市值或成本
+     */
+    getType() {
+        return this.type;
+    }
+
+    /**
+     * 取得股票證交稅金，取個位數無條件捨去(市價x3/1000)  
+     * 如果稅金金額小於1(即為0)則以最低稅金計算，最低稅金為1元新台幣
+     * @returns 稅金
+     */
+    getTaxes() {
+        let typeNumber = 0;
+        let rate = 0;
+        let taxes = 0;
+        const typeString = {
+            commonStocks: ["Common Stocks", "common stocks", "CommonStocks", "commonstocks", "CS", "cs", "0", 0],
+            dayTrading: ["Day-Trading", "day-trading", "Day Trading", "day trading", "DayTrading", "daytrading", "Dt", "dt", "1", 1],
+            exchangeTradedFunds: ["Exchange Traded Funds", "exchange traded funds", "ExchangeTradedFunds", "exchangetradedfunds", "ETF", "etf", "2", 2]
+        };
+        if (typeString.commonStocks.includes(this.type)) {
+            typeNumber = 0;
+        } else if (typeString.dayTrading.includes(this.type)) {
+            typeNumber = 1;
+        } else if (typeString.exchangeTradedFunds.includes(this.type)) {
+            typeNumber = 2;
+        } else {
+            typeNumber = -1;
+        }
+        switch (typeNumber) {
+            case 0:
+                rate = 3;
+                break;
+            case 1:
+                rate = 1.5;
+                break;
+            case 2:
+                rate = 1;
+                break;
+            default:
+                rate = -1;
+                break;
+        }
+        if (rate === -1) {
+            return "輸入錯誤";
+        }
+        taxes = Math.floor((this.getMarketValue() * rate / 1000));
+        return taxes == 0 ? 1 : taxes;
+    }
+
+    /**
+     * 取得成交均價，取小數點後2位四捨五入((買進成交價金+手續費)/股數)
+     * @returns 成交均價
+     */
+    getAveragePrice() {
+        return Math.round(((Math.round(((this.getCostPrice() + this.getCharge(this.chargeDiscount)) * 100) / 100)) / this.numberOfPiles) * 100) / 100;
+    }
+
+    /**
+     * 取得持有成本，取個位數四捨五入(買進成交價金+手續費)
+     * @returns 持有成本
+     */
+    getCarryingCosts() {
+        return Math.round(((this.getCostPrice() + this.getCharge(this.chargeDiscount)) * 100) / 100);
+    }
+
+    /**
+     * 取得預估收入，取個位數四捨五入(市值-手續費-稅金)
+     * @returns 預估收入
+     */
+    getAnticipatedRevenue() {
+        return Math.round(((this.getMarketValue() - this.getCharge(this.chargeDiscount) - this.getTaxes(this.marketPrice, this.type)) * 100) / 100);
+    }
+
+    /**
+     * 取得應付損益(預估收入-持有成本)
+     * 取得預估收入，取個位數四捨五入(市值-手續費-稅金)
+     * @returns 應付損益
+     */
+    getProfitAndLoss() {
+        return this.getAnticipatedRevenue() - this.getCarryingCosts();
+    }
+
+    /**
+     * 取得報酬率(預估收入-持有成本)
+     * 取得預估收入，取個位數四捨五入(市值-手續費-稅金)
+     * @returns 報酬率
+     */
+    getProfitAndLossPercentage() {
+        return Math.round(this.getProfitAndLoss() / this.getCarryingCosts() * 100 * 100) / 100;
+    }
+
+}
+
+
+
+
+
+
+
+// console.log(getAveragePrice(112, 5, 6));
+// console.log(getCarryingCosts(37.75, 1000, 6));
+// console.log(getAnticipatedRevenue(40.5, 1000, 6));
+// console.log(getCharge(35.12, 50, 6));
+// console.log(getTaxes(112, 5, 6));
+// console.log(Math.floor((getPrice(260, 50) * 3 / 1000) * 1) / 1);
+// console.log(getProfitAndLoss2(35.12, 34.70, 50, 6));
+// console.log(getTaxes(21.75, 5, 0));
+// console.log(getCharge(554, 5, 6));
+// console.log(getCarryingCosts(22.60, 75, 0));
+// console.log(getAnticipatedRevenue(108, 5, 6, 0));
+// console.log(getProfitAndLoss(108, 112, 5, 6, 0));
+// console.log(getProfitAndLoss(155, 177.992, 260, 6, 0));
+// console.log(getProfitAndLoss(585, 578.44, 25, 6, 0));
+// console.log(getProfitAndLoss(30.1, 29.97, 600, 6, 0));
+// console.log(getProfitAndLossPercentage(40.5,37.75, 1000, 6, 0));
+// let c = new StockCalculation(600,554,5,6,);
+// console.log(c.getCostPrice());
+// console.log(c.getProfitAndLoss());
+
+// let c1 = new StockCalculation();
+// c1.setCost(83.0);
+// c1.setMarketPrice(90.5);
+// c1.setNumberOfPiles(50);
+// c1.setType(0);
+// c1.setChargeDiscount(6);
+// console.log(c1.getCostPrice());
+// console.log(c1.getCharge());
+// console.log(c1.getCarryingCosts());
+// console.log(c1.getProfitAndLoss());
+// console.log(c1.);
